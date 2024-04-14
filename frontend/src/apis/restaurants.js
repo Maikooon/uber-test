fafc6e5c7a07821d02b0b2674e564611dac38e48
kate0626 <maiko02626@gmail.com>
2024-04-15 02:02:14 +0900

    add api

---
+++ frontend/src/apis/restaurants.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { restaurantsIndex } from '../urls/index'
+
+export const fetchRestaurants = () => {
+    return axios.get(restaurantsIndex)
+        .then(res => {
+            //response の中身だけを返す
+            return res.data
+        })
+        // 例外処理を行う
+        .catch((e) => console.error(e))
+}
